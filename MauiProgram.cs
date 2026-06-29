07288ba30b0dc966315cb9bdb76eb6545131f247
sophia-li13145 <1328044391@qq.com>
2026-06-29 11:22:15 +0800

    Add production machine start workflow

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -39,6 +39,7 @@ namespace JXHLJSApp
             builder.Services.AddTransient<RoleHomePage>();
             builder.Services.AddTransient<JXHLJSApp.Pages.WorkOrders.WorkOrderTaskListPage>();
             builder.Services.AddTransient<JXHLJSApp.Pages.WorkStart.WorkStartScanPage>();
+            builder.Services.AddTransient<JXHLJSApp.Pages.WorkStart.WorkStartOrdersPage>();
             builder.Services.AddTransient<AdminViewModel>();
             builder.Services.AddTransient<LogsViewModel>();
             builder.Services.AddSingleton<LogService>();
